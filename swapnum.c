89e08307161c45b3fd6366dc42c2f0d743a04d50
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-08-28 18:54:10 +0530

    Update swapnum.c

--- swapnum.c
+++ swapnum.c
@@ -13,3 +13,22 @@ int main()
     printf("Reversed Number is: %d",rev);
     return 0;
 }
+
+
+//iF YOU WANTED TO DO IT IN DO..WHILE LOOP
+
+do{
+    mod=n%10;
+    rev = rev*10+mod;
+    n/=10;
+}while(n !=0);
+printf("Your Answer is %d",rev);
+
+
+//IF YOU WANTED TO DO IN FOR LOOP
+
+for(num;num!=0;n/10){
+    mod=num%10;
+    rev=rev*10+mod;
+}
+printf("Your Answer is %d",rev);
